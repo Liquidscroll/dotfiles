8faac97058f511e2ba30778d2e82b73fb16f7eef
Jonathan Tynan <jojaty@gmail.com>
2025-05-27 16:32:42 +1000

    feat: added spotify-launcher, starship (prompt) configs

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Dotfiles
 
 Contains my configurations for:
-- Bash
-- Shell Colours
-- Vim
+- `bash/`
+    - `.bashrc` and `setup.sh`
+    - Shared functions are found in `lib/shared.sh`
+    - Colour definitions and messaging functions are found in `lib/colours.sh`
+
+- Spotify launcher: `spotify-launcher.conf`
+- Startship prompt: `starship.toml`
--- bash/setup.sh
+++ bash/setup.sh
@@ -2,8 +2,11 @@
 
 set -eo pipefail # Exit on error
 
+# Handle running inside dotfiles/bash or just dotfiles/
 if [ -f ../lib/shared.sh ]; then
     source ../lib/shared.sh
+elif [ -f ./lib/shared.sh ]; then
+    source ./lib/shared.sh
 else
     echo "lib/shared.sh does not exist, sourcing failed..."
     exit 1
@@ -28,3 +31,8 @@ info "Symlinking shared bash libs..."
 symlink_dotfile "lib/shared.sh" "$(xdg_config_dir)/lib/shared.sh"
 symlink_dotfile "lib/colours.sh" "$(xdg_config_dir)/lib/colours.sh"
 
+
+info "Symlinking spotify-launcher configuration..."
+symlink_dotfile "spotify-launcher.conf" "$(xdg_config_dir)/spotify-launcher.conf"
+info "Symlinking Starship prompt configuration..."
+symlink_dotfile "starship.toml" "$(xdg_config_dir)/starship.toml"
